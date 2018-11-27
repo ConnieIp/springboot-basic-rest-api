b12c9b13987a1b1170c1f872209aee64a21bd144
Connie Ip <connie.ip@cargosmart.com>
2018-11-27 23:50:00 +0800

    testcase should_get_employeee_of_a_company

--- src/test/java/com/tw/apistackbase/service/CompanyServiceTest.java
+++ src/test/java/com/tw/apistackbase/service/CompanyServiceTest.java
@@ -2,8 +2,10 @@ package com.tw.apistackbase.service;
 
 import com.tw.apistackbase.model.Company;
 import com.tw.apistackbase.model.Employee;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,7 +18,9 @@ public class CompanyServiceTest {
     @Before
     public void setUp(){
         companyService = new CompanyService();
+        Company.setCounter(1);
     }
+
     @Test
     public void should_add_company_and_search_all_companies (){
         Company company=new Company("company1",2,Arrays.asList(
@@ -45,5 +49,21 @@ public class CompanyServiceTest {
         Company actual=companyService.getCompany(2);
 
         assertEquals(company2,actual);
+    }
+
+    @Test
+    public void should_get_employeee_of_a_company (){
+        List<Employee> employees1=Arrays.asList(
+                new Employee("emp1",20,"Male",8000),
+                new Employee("emp2",21,"Female",7000));
+        Company company1=new Company("company1",2,employees1);
+        Company company2=new Company("company2",1,Arrays.asList(
+                new Employee("emp3",21,"Male",9000)));
+        companyService.add(company1);
+        companyService.add(company2);
+
+        List<Employee> actual=companyService.getCompanyEmployees(1);
+
+        assertEquals(employees1,actual);
     }
 }
