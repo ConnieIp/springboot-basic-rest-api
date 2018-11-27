4a54a1f3deafc54ccac1be6356fb776ed7a6b7fb
Connie Ip <connie.ip@cargosmart.com>
2018-11-27 22:34:09 +0800

    create CompanyController, EmployeeContoller, Company, Employee

---
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -0,0 +1,9 @@
+package com.tw.apistackbase.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/companies")
+public class CompanyController {
+}
